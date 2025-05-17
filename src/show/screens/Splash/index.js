f74f3d2c930e238b9b5ae7dd49689cc19b5fa98c
Ayush Dhingra <ayush.dhingra@luminos.software>
2025-05-18 03:43:10 +0530

    added re-animated and animation for splash screen

--- src/show/screens/Splash/index.js
+++ src/show/screens/Splash/index.js
@@ -1,12 +1,37 @@
-import React from 'react';
-import {View, Image, StyleSheet} from 'react-native';
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withTiming,
+  withSpring,
+} from 'react-native-reanimated';
+import React, {useEffect} from 'react';
+import {View, StyleSheet, Dimensions} from 'react-native';
 
 import {TnB} from '@assets';
 
+const {width} = Dimensions.get('window');
+
 const Splash = () => {
+  const opacity = useSharedValue(0);
+  const scale = useSharedValue(0.7);
+
+  useEffect(() => {
+    opacity.value = withTiming(1, {duration: 1000});
+    scale.value = withSpring(1, {damping: 10});
+  }, []);
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    opacity: opacity.value,
+    transform: [{scale: scale.value}],
+  }));
+
   return (
     <View style={styles.container}>
-      <Image source={TnB} style={styles.logo} resizeMode="contain" />
+      <Animated.Image
+        source={TnB}
+        resizeMode="contain"
+        style={[styles.logo, animatedStyle]}
+      />
     </View>
   );
 };
@@ -19,8 +44,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   logo: {
-    height: 200,
-    width: 200,
+    width: width * 0.5,
+    height: width * 0.5,
   },
 });
 
